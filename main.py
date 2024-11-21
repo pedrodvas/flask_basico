37e068dd9c95c96e68f0456c463c531e92698ea3
Pedro Damasceno Vasconcellos <pedro.d.vas@gmail.com>
2024-11-20 23:18:07 -0300

    .

--- main.py
+++ main.py
@@ -13,18 +13,24 @@
 # limitations under the License.
 
 # [START gae_flex_quickstart]
-from flask import Flask, render_template, request
-import chatbot
+from flask import Flask
+
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    output = None
-    if request.method == 'POST':
-        user_input = request.form['user_input']
-        output = chatbot.process_input(user_input)  # Chama a função de processamento em main.py
-    return render_template('index.html', output=output)
+
+@app.route("/")
+def hello() -> str:
+    """Return a friendly HTTP greeting.
+
+    Returns:
+        A string with the words 'Hello World!'.
+    """
+    return "Hello World!"
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app.
+    app.run(host="127.0.0.1", port=8080, debug=True)
+# [END gae_flex_quickstart]
